35a29e67601f98304281ab826521263ae134ad08
Rizwan Siddiqui <rizwansiddiqui483@gmail.com>
2026-07-08 13:49:09 +0530

    Added student details script

---
+++ scripts/student_details.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter your Name:"
+read name
+
+echo "Enter your College:"
+read college
+
+echo "Enter your Branch:"
+read branch
+
+echo "-----------------------------"
+echo "       Student Details        "
+echo "-----------------------------"
+echo "Name    : $name"
+echo "College : $college"
+echo "Branch  : $branch"
+echo "-----------------------------"
